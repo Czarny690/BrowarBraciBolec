d4a355bdd56750d14de5a03c8993557cb9a88913
trixiee21 <jaromir.karvat@gmail.com>
2025-04-26 18:44:54 +0200

    Update strona.php

--- strona.php
+++ strona.php
@@ -6,6 +6,26 @@
     <title>Document</title>
 </head>
 <body>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <aside class="menu">
+        <nav>
+            <a href="pdw">piwo</a>
+            <a href="pdw">główna</a>
+            <a href="pdw">czat</a>
+            <a href="pdw">forum</a>
+            <a href="pdw">elo</a>
+        </nav>
+    </aside>
+</body>
+</html>
     <form method="post">
         <input type="submit" value="Wyloguj się" name="wyloguj">
     </form>
